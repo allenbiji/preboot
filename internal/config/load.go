1f016facac588a160afde718d82358fa50f76cea
allenbiji <georgeallen378@gmail.com>
2026-06-14 18:10:27 +0530

    feat: add config loader

---
+++ internal/config/load.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/allenbiji/clone-sage/internal/model"
+	"github.com/spf13/viper"
+)
+
+func Load() (*model.ClonesageConfig, error) {
+	v := viper.New()
+
+	v.SetConfigFile("sage-auto.yaml")
+
+	err:= v.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, fmt.Errorf("Error reading config file: %w", err)
+		}
+	}
+
+	v.SetConfigFile("sage.yaml")
+	if err := v.MergeInConfig(); err != nil{
+		return nil, fmt.Errorf("Error merging config files: %w", err)
+	}
+
+	var cfg model.ClonesageConfig
+	if err := v.Unmarshal(&cfg); err != nil{
+		return nil, fmt.Errorf("There was an error in unmarshaling the yaml config: %w", err)
+	}
+
+	if err := ValidateConfig(&cfg); err != nil {
+		return nil, fmt.Errorf("There was an error in validating the yaml configs: %w", err)
+	}
+
+	return &cfg, nil
+}
