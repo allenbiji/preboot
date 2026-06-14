728d2c8f52c3149e69616720e4b22db5c4c73d59
allenbiji <georgeallen378@gmail.com>
2026-06-14 18:13:51 +0530

    feat: add validate command

--- internal/cli/validate.go
+++ internal/cli/validate.go
@@ -3,16 +3,22 @@ package cli
 import (
 	"fmt"
 
+	"github.com/allenbiji/clone-sage/internal/config"
 	"github.com/spf13/cobra"
 )
 
-func NewValidateCmd() *cobra.Command{
+// this is the command used to validate the user-defined checks
+func NewValidateCmd() *cobra.Command {
 	validCmd := &cobra.Command{
-		Use: "validate",
+		Use:   "validate",
 		Short: "Used to validate the checks in sage.yaml",
-		Long: "This command can be to used to verify if the user-defined checks in sage.yaml are valid or not",
+		Long:  "This command can be to used to verify if the user-defined checks in sage.yaml are valid or not",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Validate command has been invoked")
+			if _, err := config.Load(); err != nil {
+				return err
+			}
+
+			fmt.Println("Configuration is valid!")
 			return nil
 		},
 	}
